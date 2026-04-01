aee9868c0fcbe7555353a8deea29a0eefbe0e916
LuisEduSouza <luisduduovaes@gmail.com>
2026-04-01 16:55:47 -0300

    Terminando lógica de transformar um numero em decimal

--- conversor.ts
+++ conversor.ts
@@ -1,44 +1,77 @@
 console.clear();
 
-let numero: number;
-let base: number;
-let numerosArray: number[] = [];
 const teclado = require("prompt-sync")();
 
-numero = parseInt(teclado(`Digite o número:`));
-base = parseInt(teclado(`Digite a base:`))
+let entrada: string;
+let base: number;
 
-//Desestruturando número em um array
-while (numero != 0) {
-    let digito: number = numero % 10;
-    numero = (numero - digito) / 10;
-    console.log(`numero é igual ${numero}`);
-    console.log(`digito é igual ${digito}`);
-    numerosArray.push(digito);
+let baseFinal: number;
+
+entrada = teclado(`Digite o número: `);
+base = parseInt(teclado(`Digite a base: `));
+baseFinal = parseInt(teclado(`Digite a base deseja para passar o seu número `));
+
+function transformarDecimal(entrada: string, base: number): number {
+    let numerosArray: number[] = [];
+    let decimal: number = 0;
+    let numero: number;
+
+    if (base != 10) {
+        if (base <= 9) {
+            numero = parseInt(entrada);
+            while (numero != 0) {
+                let digito: number = numero % 10;
+                numero = (numero - digito) / 10;
+                numerosArray.push(digito);
+            }
+        }
+
+        else if (base == 16) {
+            let hexaDecimal: string = entrada.toUpperCase();
+            for (let i = hexaDecimal.length - 1; i >= 0; i--) {
+                switch (hexaDecimal[i]) {
+                    case "A":
+                        numerosArray.push(10);
+                        break;
+                    case "B":
+                        numerosArray.push(11);
+                        break;
+                    case "C":
+                        numerosArray.push(12);
+                        break;
+                    case "D":
+                        numerosArray.push(13);
+                        break;
+                    case "E":
+                        numerosArray.push(14);
+                        break;
+                    case "F":
+                        numerosArray.push(15);
+                        break;
+                    default:
+                        numerosArray.push(parseInt(hexaDecimal[i]!));
+                }
+            }
+        }
+
+        let valorPosicional: number[] = [];
+
+        for (let i = 0; i < numerosArray.length; i++) {
+            let expoente: number;
+            expoente = Math.pow(base, i);
+            valorPosicional[i] = numerosArray[i]! * expoente;
+            decimal += valorPosicional[i]!;
+        }
+
+        return decimal;
+    } else {
+        decimal = parseInt(entrada);
+        return decimal;
+    }
 }
 
-console.log(numerosArray);
+let resultado = transformarDecimal(entrada, base);
 
-//Contador do tamanho do array
-let tamanho: number = 0;
-while (numerosArray[tamanho] !== undefined) {
-    console.log(numerosArray[tamanho]);
-    tamanho++;
-}
-console.log(`Tamanho do Array: ${tamanho}`);
+console.log(resultado);
 
-let teste: number[] = [];
-let soma: number = 0;
 
-console.log(`Array inteiro invertido`, numerosArray);
-for (let i = 0; i < tamanho; i++) {
-    console.log(`Espaço ${i} do Array`, numerosArray[i]);
-    let expoente: number;
-    expoente = Math.pow(base, i);
-    console.log(`O resultado do expoente é: ${expoente}`);
-    teste[i] = numerosArray[i]! * expoente;
-    soma += teste[i]!;
-}
-
-console.log(teste);
-console.log(soma);
